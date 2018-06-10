6b1615306b8697f25df9dd59515e3cba5233fd97
pamelagao <pamela.gao@hotmail.com>
2018-06-10 01:46:43 -0400

    add courses model

---
+++ collaboration/src/app/courses.ts
@@ -0,0 +1,8 @@
+export class Courses {
+    id          : number
+    name        : string
+    start       : boolean
+    start_time  : Date
+    end_time    : Date
+    instructor  : [string, any]
+}
