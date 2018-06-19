bf659a4d0ae78182f80350eec3d0337ed7491511
pamelagao <pamela.gao@hotmail.com>
2018-06-19 12:46:49 -0400

    part one

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Angular2 Collaboration
+
+To demonstrate rudimentary knowledge of the Angular 2+ framework.
+
+The task is to develop component, service, and model by Angular. And the model must have
+at least two differently-typed properties that are not string nor number.
--- collaboration/src/app/courses.service.ts
+++ collaboration/src/app/courses.service.ts
@@ -10,7 +10,7 @@ export class CoursesService {
 
   courses : Courses[] = [
     { id : 1, name : 'Portfolio Development', start : true, start_time : new Date('2018-06-10T08:55:00'), end_time : new Date('2018-06-10T12:30:00'), instructor : ['Joanna', 'Joanna@humber.ca'] },
-    { id : 2, name : 'Web Project', start : true, start_time : new Date('2018-06-10T08:00'), end_time : new Date('2018-06-10T11:35:00'), instructor : ['Nithya', 'Nithya@humber.ca'] },
+    { id : 2, name : 'Web Project', start : true, start_time : new Date('2018-06-10T08:00:00'), end_time : new Date('2018-06-10T11:35:00'), instructor : ['Nithya', 'Nithya@humber.ca'] },
     { id : 3, name : 'Usability and Accessibility', start : false, start_time : new Date('2018-06-10T08:55:00'), end_time : new Date('2018-06-10T12:30:00'), instructor : ['Simon', 'Simon@humber.ca'] }
   ];
   
